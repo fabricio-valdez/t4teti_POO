4f6872082c51ede3ed86c5ddd048098d267d54b4
Fabricio Valdez <fabriciovaldezz41@gmail.com>
2022-08-30 11:07:17 -0300

    background update

--- public/t4teti.js
+++ public/t4teti.js
@@ -1,4 +1,6 @@
-const btnPulsado = () => {
-    alert('Hola!')
+const btnPanel = (panel) => {
+    const btn= panel.target;
+    btn.style.backgroundColor = 'red'
+    
 }
-document.querySelectorAll('button').forEach(obj => obj.addEventListener('click',btnPulsado));
+document.querySelectorAll('button').forEach(obj => obj.addEventListener('click',btnPanel));
