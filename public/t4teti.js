88a002bb32dcc8d8121538e7e3e1584ed229c08b
Fabricio Valdez <fabriciovaldezz41@gmail.com>
2022-08-30 11:24:15 -0300

    paneles

--- public/t4teti.js
+++ public/t4teti.js
@@ -1,6 +1,31 @@
-const btnPanel = (panel) => {
+const Paneles = [];
+let turno = 0;
+const btnPanel = (panel, posicion) => {
+    turno+=1;
     const btn= panel.target;
-    btn.style.backgroundColor = 'red'
-    
+    const jugador = turno % 2 ? 'red' : 'green'; 
+    btn.style.backgroundColor = jugador;
+    Paneles [posicion] = jugador;
+    alert (juego())
 }
-document.querySelectorAll('button').forEach(obj => obj.addEventListener('click',btnPanel));
+const juego = () =>{
+    if (Paneles[0] == Paneles[1] && Paneles[0] == Paneles[2] && Paneles[0]){
+        return true;
+    }else if (Paneles[3] == Paneles[4] && Paneles[3] == Paneles[5] && Paneles[3]){
+        return true;
+    }else if (Paneles[6] == Paneles[7] && Paneles[6] == Paneles[8] && Paneles[6]){
+        return true
+    }else if (Paneles[0] == Paneles[3] && Paneles[0] == Paneles[6] && Paneles[0]){
+        return true
+    }else if (Paneles[1] == Paneles[4] && Paneles[1] == Paneles[7] && Paneles[1]){
+        return true
+    }else if (Paneles[2] == Paneles[5] && Paneles[2] == Paneles[8] && Paneles[2]){
+        return true
+    }else if (Paneles[0] == Paneles[4] && Paneles[0] == Paneles[8] && Paneles[0]){
+        return true
+    }else if (Paneles[2] == Paneles[4] && Paneles[2] == Paneles[6] && Paneles[2]){
+        return true
+    }
+    return false
+}
+document.querySelectorAll('button').forEach ((obj,i) => obj.addEventListener('click',(panel) => btnPanel (panel,i)));
