ed1efe833e389ce21a4099d50d3f9db674cbaaf6
Fabricio Valdez <fabriciovaldezz41@gmail.com>
2022-09-01 00:51:14 -0300

    update

--- public/t4teti.js
+++ public/t4teti.js
@@ -9,23 +9,42 @@ const btnPanel = (panel, posicion) => {
     if(juego())alert('Gano el jugador ' + jugador);
 }
 const juego = () =>{
-    if (Paneles[0] == Paneles[1] && Paneles[0] == Paneles[2] && Paneles[0]){
-        return true;
-    }else if (Paneles[3] == Paneles[4] && Paneles[3] == Paneles[5] && Paneles[3]){
-        return true;
-    }else if (Paneles[6] == Paneles[7] && Paneles[6] == Paneles[8] && Paneles[6]){
+    if ((Paneles[0] == Paneles[1] && Paneles[0] == Paneles[2] && Paneles[0])||
+    (Paneles[3] == Paneles[4] && Paneles[3] == Paneles[5] && Paneles[3])||
+    (Paneles[6] == Paneles[7] && Paneles[6] == Paneles[8] && Paneles[6])||
+    (Paneles[0] == Paneles[3] && Paneles[0] == Paneles[6] && Paneles[0])||
+    (Paneles[1] == Paneles[4] && Paneles[1] == Paneles[7] && Paneles[1])||
+    (Paneles[2] == Paneles[5] && Paneles[2] == Paneles[8] && Paneles[2])||
+    (Paneles[0] == Paneles[4] && Paneles[0] == Paneles[8] && Paneles[0])||
+    (Paneles[2] == Paneles[4] && Paneles[2] == Paneles[6] && Paneles[2])){
+        reset()
+        Paneles.forEach((i)=>{Paneles.splice(i)})
         return true
-    }else if (Paneles[0] == Paneles[3] && Paneles[0] == Paneles[6] && Paneles[0]){
-        return true
-    }else if (Paneles[1] == Paneles[4] && Paneles[1] == Paneles[7] && Paneles[1]){
-        return true
-    }else if (Paneles[2] == Paneles[5] && Paneles[2] == Paneles[8] && Paneles[2]){
-        return true
-    }else if (Paneles[0] == Paneles[4] && Paneles[0] == Paneles[8] && Paneles[0]){
-        return true
-    }else if (Paneles[2] == Paneles[4] && Paneles[2] == Paneles[6] && Paneles[2]){
-        return true
-    }
+    }else if(empate()) {
+        reset()
+        Paneles.forEach((i)=>{Paneles.splice(i)})
+        alert('Empate')
+    }else { 
     return false
+    }
 }
 document.querySelectorAll('button').forEach ((obj,i) => obj.addEventListener('click',(panel) => btnPanel (panel,i)));
+function empate(){
+    cont = 0
+    for (i=0;i<9;i++){
+        if(document.getElementById(i).style.backgroundColor=="red"|| document.getElementById(i).style.backgroundColor=='green'){
+            cont+=1}}
+    if (cont == 9){
+        return true
+    }else{
+        return false
+    }
+}
+function reset(){
+    for (i=0;i<9;i++){
+        document.getElementById(i).style.background = ''
+    }
+}
+function color(id){
+    return document.getElementById(id).style.background
+}
